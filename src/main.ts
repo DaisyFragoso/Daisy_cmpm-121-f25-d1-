ae91cd94c906bfb71a601b97142049dd2dbf66f4
DaisyFragoso <87869666+DaisyFragoso@users.noreply.github.com>
2025-12-03 12:11:42 +0000

    d1.a (step 5  complete) added: another buttom to simulate as the upgrade. added a growthrate so it shows growth to each upgraded. Upgraded tested with 10 increase it is now set to +1. The button has a limit by making it disabled when players cookies is less than 10 since they cant afford it. the inital growth rate is also set to 0 so it doesnt continously growth when then button have never been clicked

--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,56 @@
 import cookiebuttonImgUrl from "./cookie.png";
 import "./style.css";
 
+//initial stuff
 document.body.innerHTML = `
   <p>🍪 <span id="counter">0</span> Cookies collected! </p>
 `;
 
+//inital button
 const button = document.createElement("button");
 button.innerHTML =
   `<img src="${cookiebuttonImgUrl}" alt="button icon" style="width:40px; height:40px;" />`;
 button.style.padding = "0.5rem 1rem";
-
 document.body.appendChild(button);
 
+//milk purchase button
+const upgradeButton = document.createElement("button");
+upgradeButton.textContent = "🥛 want milk? (Cost 10) (+1 per second)";
+upgradeButton.disabled = true;
+document.body.appendChild(upgradeButton);
+
 let count = 0;
+
+let growthRate = 0;
+
 const counter = document.getElementById("counter") as HTMLSpanElement;
 
+//update display
 function updatecounterDisplayHelper() {
   counter.textContent = count.toFixed(1);
 }
 
+//inital button increase
 button.addEventListener("click", () => {
   if (!counter) return;
   count++;
   updatecounterDisplayHelper();
 });
+
+//milk upgrade button
+upgradeButton.addEventListener("click", () => {
+  if (count >= 10) {
+    count -= 10;
+    growthRate += 1;
+    updatecounterDisplayHelper();
+    upgradeEnableDisableHelper();
+  }
+});
+
+//enables and disables the upgrade button
+function upgradeEnableDisableHelper() {
+  upgradeButton.disabled = count < 10;
+}
 
 //step 4
 let lastTimestamp: number | null = null;
@@ -41,8 +68,10 @@ function animate(timestamp: number) {
 
     const deltaSeconds = deltaMs / 1000;
 
-    count += deltaSeconds;
+    count += growthRate * deltaSeconds;
+
     updatecounterDisplayHelper();
+    upgradeEnableDisableHelper();
   }
 
   requestAnimationFrame(animate);
