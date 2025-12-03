1675e86e84b760b6b5ba8d88ffc9032a4313fcdb
DaisyFragoso <87869666+DaisyFragoso@users.noreply.github.com>
2025-12-03 10:59:10 +0000

    d1.a (step 3 complete) added: a updatecounterDisplayhelper() so every second it goes through this function to increase the cookies clicked by 1 every 1 second. in the button.event listener it also goes to the same function, not directly towards updated the string - this hopefully cleans up code repeats

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import cookiebuttonImgUrl from "./cookie.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>🍪 <span id="counter">0</span> Cookies clicked! </p>
+  <p>🍪 <span id="counter">0</span> Cookies collected! </p>
 `;
 
 const button = document.createElement("button");
@@ -17,10 +17,20 @@ button.style.padding = "0.5rem 1rem";
 document.body.appendChild(button);
 
 let count = 0;
-const counter = document.getElementById("counter") as HTMLSpanElement | null;
+const counter = document.getElementById("counter") as HTMLSpanElement;
+
+function updatecounterDisplayHelper() {
+  counter.textContent = count.toString();
+}
 
 button.addEventListener("click", () => {
   if (!counter) return;
   count++;
-  counter.textContent = count.toString();
+  updatecounterDisplayHelper();
 });
+
+//step 3 increases every second
+setInterval(() => {
+  count += 1;
+  updatecounterDisplayHelper();
+}, 1000);
